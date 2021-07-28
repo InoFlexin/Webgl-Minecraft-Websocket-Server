ad8f8ba9b993fa7f3eb5c71fd06c10a8c1abfecf
DaeYoung Nam <wsnam0507@naver.com>
2021-07-28 19:09:00 +0900

    feat: Message Broadcaster의 전처리(미들웨어) 구현 및 정의

--- src/main/java/net/metaclass/domain/broadcast/middleware/BroadcasterMiddleware.java
+++ src/main/java/net/metaclass/domain/broadcast/middleware/BroadcasterMiddleware.java
@@ -1,7 +1,10 @@
 package net.metaclass.domain.broadcast.middleware;
 
-public interface SessionMiddleware {
+import net.metaclass.domain.message.protocol.MessageWrapper;
+import net.metaclass.domain.session.SessionContext;
 
-    
+public interface BroadcasterMiddleware {
+
+    void bridge(MessageWrapper wrapper, SessionContext sessionContext);
 
 }
--- src/main/java/net/metaclass/domain/broadcast/middleware/SessionMiddlewareImpl.java
+++ src/main/java/net/metaclass/domain/broadcast/middleware/SessionMiddlewareImpl.java
@@ -1,2 +1,35 @@
-package net.metaclass.domain.broadcast.middleware;public class SessionMiddlewareImpl {
+package net.metaclass.domain.broadcast.middleware;
+
+import net.metaclass.domain.message.CloseMessage;
+import net.metaclass.domain.message.TextMessage;
+import net.metaclass.domain.message.protocol.Message;
+import net.metaclass.domain.message.protocol.MessageWrapper;
+import net.metaclass.domain.session.SessionContext;
+
+public class SessionMiddlewareImpl implements BroadcasterMiddleware {
+
+    @Override
+    public void bridge(MessageWrapper wrapper, SessionContext sessionContext) {
+        Message message = wrapper.getMessage();
+
+        if(message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+
+            if(!sessionContext.existsClass(textMessage.getClassName())) {
+                sessionContext.registerClassSession(textMessage.getClassName(), wrapper.getChannel());
+            }
+            if(!sessionContext.existsSession(wrapper.getChannel())) {
+                sessionContext.registerUserSession(wrapper.getChannel(), textMessage.getClassName());
+            }
+        }
+        else if(message instanceof CloseMessage) {
+            CloseMessage closeMessage = (CloseMessage) message;
+
+            if(sessionContext.existsSession(wrapper.getChannel())) {
+                sessionContext.disconnect(wrapper.getChannel());
+                System.out.println("Closed channel: " + closeMessage.toString());
+            }
+        }
+    }
+
 }

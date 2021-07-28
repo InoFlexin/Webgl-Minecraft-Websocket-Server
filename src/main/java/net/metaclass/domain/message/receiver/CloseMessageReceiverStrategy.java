8dd3c58ac595539b71e59aa1b3d1cac189526043
DaeYoung Nam <wsnam0507@naver.com>
2021-07-28 18:06:44 +0900

    refactor: 반환타입을 MessageWrapper로 변경함으로 인해 코드 수정

--- src/main/java/net/metaclass/domain/message/receiver/CloseMessageReceiverStrategy.java
+++ src/main/java/net/metaclass/domain/message/receiver/CloseMessageReceiverStrategy.java
@@ -4,15 +4,18 @@ import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
 import net.metaclass.domain.message.CloseMessage;
 import net.metaclass.domain.message.protocol.Message;
 import net.metaclass.domain.message.protocol.MessageReceiver;
+import net.metaclass.domain.message.protocol.MessageWrapper;
 import net.metaclass.domain.message.protocol.WebSocketFrameWrapper;
 
 public class CloseMessageReceiverStrategy implements MessageReceiver<CloseMessage> {
 
     @Override
-    public CloseMessage receive(WebSocketFrameWrapper webSocketFrameWrapper) {
+    public MessageWrapper<CloseMessage> receive(WebSocketFrameWrapper webSocketFrameWrapper) {
         CloseWebSocketFrame closeWebSocketFrame = (CloseWebSocketFrame) webSocketFrameWrapper.getWebSocketFrame();
         Message<CloseMessage> message = new CloseMessage(closeWebSocketFrame.reasonText(), closeWebSocketFrame.statusCode());
+        MessageWrapper<CloseMessage> messageWrapper =
+                new MessageWrapper<>(message, webSocketFrameWrapper.getChannel());
 
-        return message.get();
+        return messageWrapper;
     }
 }
--- src/main/java/net/metaclass/domain/message/receiver/TextMessageReceiverStrategy.java
+++ src/main/java/net/metaclass/domain/message/receiver/TextMessageReceiverStrategy.java
@@ -4,16 +4,19 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import net.metaclass.domain.message.TextMessage;
 import net.metaclass.domain.message.protocol.Message;
 import net.metaclass.domain.message.protocol.MessageReceiver;
+import net.metaclass.domain.message.protocol.MessageWrapper;
 import net.metaclass.domain.message.protocol.WebSocketFrameWrapper;
 
 public class TextMessageReceiverStrategy implements MessageReceiver<TextMessage> {
 
     @Override
-    public TextMessage receive(WebSocketFrameWrapper webSocketFrameWrapper) {
+    public MessageWrapper<TextMessage> receive(WebSocketFrameWrapper webSocketFrameWrapper) {
         String text = ((TextWebSocketFrame) webSocketFrameWrapper.getWebSocketFrame()).text();
         Message<TextMessage> textMessage = new TextMessage().parse(text);
+        MessageWrapper<TextMessage> messageWrapper =
+                new MessageWrapper<>(textMessage, webSocketFrameWrapper.getChannel());
 
-        return textMessage.get();
+        return messageWrapper;
     }
 
 }

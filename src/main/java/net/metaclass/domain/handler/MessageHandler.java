b7f53751205da86eff325cffc05f6d46c7d7a6bf
DaeYoung Nam <wsnam0507@naver.com>
2021-07-28 17:44:12 +0900

    feat: MessageHandler 인터페이스 정의

--- src/main/java/net/metaclass/domain/handler/MessageHandler.java
+++ src/main/java/net/metaclass/domain/handler/MessageHandler.java
@@ -1,2 +1,9 @@
-package net.metaclass.domain.handler;public class MessageHandler {
+package net.metaclass.domain.handler;
+
+import net.metaclass.domain.message.protocol.MessageWrapper;
+
+public interface MessageHandler<T> {
+
+    void handleMessage(MessageWrapper<T> wrapper);
+
 }

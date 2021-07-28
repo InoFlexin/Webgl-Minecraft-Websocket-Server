8592ee786d717ccb2a0a391386e7699716560fbf
DaeYoung Nam <wsnam0507@naver.com>
2021-07-28 15:15:32 +0900

    test: gson message 변환 테스트

--- src/test/java/com/metaclass/test/MessageJsonTest.java
+++ src/test/java/com/metaclass/test/MessageJsonTest.java
@@ -1,4 +1,28 @@
 package com.metaclass.test;
 
+import net.metaclass.domain.Message;
+import net.metaclass.domain.TextMessage;
+import net.metaclass.domain.data.DataContext;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MessageJsonTest {
+
+    @Test
+    public void messageJsonConvertTest() {
+        DataContext dataContextBuilder = DataContext.builder();
+        dataContextBuilder.setData("x", 1.0);
+        dataContextBuilder.setData("y", 2.5);
+        dataContextBuilder.setData("z", 3.1);
+        Message<TextMessage> message = new TextMessage("onPlayerMove", "id", "example_class", dataContextBuilder.to());
+        String json = message.toJson();
+        System.out.println(json);
+
+        Message<TextMessage> fromJson = new TextMessage().parse(json);
+
+        assertEquals(fromJson != null, true);
+        assertEquals(json, fromJson.toJson());
+    }
+
 }

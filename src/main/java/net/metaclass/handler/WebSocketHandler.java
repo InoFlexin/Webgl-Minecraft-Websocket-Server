f8461d0aa28f591e525b6f8bb11f1af43938d892
DaeYoung Nam <wsnam0507@naver.com>
2021-07-28 17:51:50 +0900

    refactor: Receiver 반환값이 MessageWrapper로 변경됨에 따라 받는 변수 타입을 Object -> MessageWrapper로 변경

--- src/main/java/net/metaclass/handler/WebSocketHandler.java
+++ src/main/java/net/metaclass/handler/WebSocketHandler.java
@@ -3,6 +3,8 @@ package net.metaclass.handler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.websocketx.*;
+import net.metaclass.domain.message.exeception.UnsupportedWebSocketFrameException;
+import net.metaclass.domain.message.protocol.MessageWrapper;
 import net.metaclass.domain.message.protocol.WebSocketFrameWrapper;
 import net.metaclass.domain.message.receiver.MessageReceiverFactory;
 import net.metaclass.domain.message.protocol.MessageReceiver;
@@ -12,11 +14,15 @@ public class WebSocketHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if(msg instanceof WebSocketFrame) {
-            WebSocketFrame webSocketFrame = (WebSocketFrame) msg;
-            MessageReceiver receiver = MessageReceiverFactory.getFactory().getReceiver(webSocketFrame);
-            WebSocketFrameWrapper webSocketFrameWrapper = new WebSocketFrameWrapper(ctx.channel(), webSocketFrame);
-            Object dto = receiver.receive(webSocketFrameWrapper);
-
+            try {
+                WebSocketFrame webSocketFrame = (WebSocketFrame) msg;
+                MessageReceiver receiver = MessageReceiverFactory.getFactory().getReceiver(webSocketFrame);
+                WebSocketFrameWrapper webSocketFrameWrapper = new WebSocketFrameWrapper(ctx.channel(), webSocketFrame);
+                MessageWrapper dto = receiver.receive(webSocketFrameWrapper);
+            } catch (UnsupportedWebSocketFrameException e) {
+                e.printStackTrace();
+                // TODO: 에러 처리
+            }
         }
     }
 }
---
+++ src/test/java/com/metaclass/test/MessageReceiverTest.java
@@ -0,0 +1,2 @@
+package com.metaclass.test;public class MessageReceiverTest {
+}

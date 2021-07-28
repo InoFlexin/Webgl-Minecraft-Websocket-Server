3d34c7d0a8fec6f97b9167a9a20104d6972857ec
DaeYoung Nam <wsnam0507@naver.com>
2021-07-28 17:44:59 +0900

    feat: Message Type에 따라 각각의 handler를 반환해주는 Factory 클래스 구현

--- src/main/java/net/metaclass/domain/handler/MessageHandlerFactory.java
+++ src/main/java/net/metaclass/domain/handler/MessageHandlerFactory.java
@@ -1,2 +1,36 @@
-package net.metaclass.domain.handler;public class MessageHandlerFactory {
+package net.metaclass.domain.handler;
+
+import net.metaclass.domain.message.CloseMessage;
+import net.metaclass.domain.message.TextMessage;
+import net.metaclass.domain.message.protocol.Message;
+import net.metaclass.domain.message.protocol.MessageWrapper;
+
+import java.util.Optional;
+
+public class MessageHandlerFactory {
+
+    private static MessageHandlerFactory factory;
+
+    static public synchronized MessageHandlerFactory getFactory() {
+        if(factory == null) {
+            factory = new MessageHandlerFactory();
+        }
+        return factory;
+    }
+
+    private MessageHandlerFactory() {}
+
+    public <T> Optional<MessageHandler> getHandler(MessageWrapper<T> wrapper) {
+        Message message = wrapper.getMessage();
+
+        if(message instanceof TextMessage) {
+            return Optional.of(new TextMessageHandler());
+        }
+        else if(message instanceof CloseMessage) {
+            return Optional.of(new CloseMessageHandler());
+        }
+
+        return Optional.empty();
+    }
+
 }

6b58e8d99f933e043f49746ae50bcc06e1a4f614
DaeYoung Nam <wsnam0507@naver.com>
2021-07-28 19:09:35 +0900

    feat: MessageBroadcaster 구현

--- src/main/java/net/metaclass/domain/broadcast/MessageBroadcaster.java
+++ src/main/java/net/metaclass/domain/broadcast/MessageBroadcaster.java
@@ -1,2 +1,57 @@
-package net.metaclass.domain.broadcast;public interface MessageBroadcaster {
+package net.metaclass.domain.broadcast;
+
+import io.netty.channel.Channel;
+import net.metaclass.domain.broadcast.middleware.BroadcasterMiddleware;
+import net.metaclass.domain.message.TextMessage;
+
+import net.metaclass.domain.message.protocol.MessageWrapper;
+import net.metaclass.domain.session.SessionContext;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MessageBroadcaster {
+
+    private SessionContext sessionContext;
+    private Set<BroadcasterMiddleware> middlewareSet;
+
+    public MessageBroadcaster(SessionContext sessionContext, BroadcasterMiddleware... middlewares) {
+        this.sessionContext = sessionContext;
+        this.middlewareSet = new HashSet<>();
+
+        initialize(middlewares);
+    }
+
+    private void initialize(BroadcasterMiddleware[] middlewares) {
+        for(BroadcasterMiddleware middleware : middlewares) {
+            middlewareSet.add(middleware);
+        }
+    }
+
+    private void runMiddleWares(MessageWrapper messageWrapper) {
+        for(BroadcasterMiddleware middleware : middlewareSet) {
+            middleware.bridge(messageWrapper, sessionContext);
+        }
+    }
+
+    public void broadcast(MessageWrapper messageWrapper) {
+        runMiddleWares(messageWrapper);
+
+        if(messageWrapper.getMessage() instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) messageWrapper.getMessage();
+            String className = textMessage.getClassName();
+            String json = textMessage.toJson();
+            List<Channel> channels = sessionContext.getChannelsInClass(className);
+
+            broadcast(channels, json);
+        }
+    }
+
+    public void broadcast(List<Channel> channels, String data) {
+        for(Channel channel : channels) {
+            channel.write(data);
+        }
+    }
+
 }

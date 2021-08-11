04ed404f42f0d0ef5745d15168d637e5883be029
DaeYoung Nam <wsnam0507@naver.com>
2021-08-12 02:48:42 +0900

    feat: AbstractMessageReceiver에 채널과 웹소켓 프레임을 받는 receive 구현, 반환은 구현체에서 메소드 실행 후 메세지 반환

--- src/main/java/net/metaclass/domain/message/protocol/AbstractMessageReceiver.java
+++ src/main/java/net/metaclass/domain/message/protocol/AbstractMessageReceiver.java
@@ -1,2 +1,12 @@
-package net.metaclass.domain.message.protocol;public class AbstractMessageReceiver {
+package net.metaclass.domain.message.protocol;
+
+import io.netty.channel.Channel;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+
+public abstract class AbstractMessageReceiver<T> implements MessageReceiver<T> {
+
+    public MessageWrapper<T> receive(Channel channel, WebSocketFrame webSocketFrame) {
+        return receive(new WebSocketFrameWrapper(channel, webSocketFrame));
+    }
+
 }

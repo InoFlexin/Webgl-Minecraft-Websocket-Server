163781ff10101efcaabdfc7d46973093df441103
DaeYoung Nam <wsnam0507@naver.com>
2021-08-12 02:47:05 +0900

    fix: 안쓰는 import 지움

--- src/main/java/net/metaclass/handler/WebSocketHandler.java
+++ src/main/java/net/metaclass/handler/WebSocketHandler.java
@@ -2,7 +2,6 @@ package net.metaclass.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.group.ChannelGroup;
 import io.netty.handler.codec.http.websocketx.*;
 import net.metaclass.domain.broadcast.MessageBroadcaster;
 import net.metaclass.domain.broadcast.middleware.SessionMiddlewareImpl;
@@ -10,17 +9,12 @@ import net.metaclass.domain.handler.MessageHandler;
 import net.metaclass.domain.message.exeception.UnsupportedWebSocketFrameException;
 import net.metaclass.domain.message.protocol.AbstractMessageReceiver;
 import net.metaclass.domain.message.protocol.MessageWrapper;
-import net.metaclass.domain.message.protocol.WebSocketFrameWrapper;
 import net.metaclass.domain.message.receiver.MessageReceiverFactory;
-import net.metaclass.domain.message.protocol.MessageReceiver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class WebSocketHandler extends ChannelInboundHandlerAdapter {
 
     private MessageBroadcaster messageBroadcaster;
     private MessageHandler messageHandler;
-
     public WebSocketHandler() {
         this.messageBroadcaster = new MessageBroadcaster(new SessionMiddlewareImpl());
         this.messageHandler = new MessageHandler(messageBroadcaster);
